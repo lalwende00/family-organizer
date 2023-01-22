77c7b25e5ede9e327e39354883a1022a450074f1
SBO <sarahdev808@gmail.com>
2023-01-22 20:37:29 +0100

    add modal

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import AlternateDays from "../../interfaces/alternateDays";
 import Modal from "./Modals/Modal";
 import UseModal from "./Modals/UseModal";
+
 type Props = {};
 
 function Calendar({}: Props) {
@@ -122,9 +123,11 @@ function Calendar({}: Props) {
       <div className="p-4 h-[420px] bg-white shadow-lg rounded-2xl dark:bg-gray-700">
         <div className="flex flex-wrap overflow-hidden">
           <div className="w-full rounded shadow-sm">
-          <button data-modal-target="staticModal" data-modal-toggle="staticModal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-          Toggle modal
-          </button>
+          <button
+		className="bg-green-500 text-white rounded-md px-8 py-2 text-base font-medium hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+		id="open-btn"
+	>	Open Modal
+	</button>
           <Modal></Modal>
             <div className="flex items-center justify-between mb-4">
               <div className="text-xl font-bold text-left text-black dark:text-white">
@@ -196,14 +199,14 @@ function Calendar({}: Props) {
                     (dayOfCurrentMonth: number) => {
                       if (dayOfCurrentMonth === day) {
                         return (
+                          <div>
                           <span className="px-2 py-3 text-center text-white cursor-pointer ">
-                            <span onClick={toggle} className="p-2 bg-blue-500 rounded-full">
+                            <span className="p-2 bg-blue-500 rounded-full">
                               {dayOfCurrentMonth}
                             </span>
-                            <Modal isOpen={isOpen} toggle={toggle}>
-                          
-                        </Modal>
-                          </span>
+                          </span>                       
+                          <Modal/>
+                          </div>
                         );
                       } else {
                         return (
@@ -211,39 +214,6 @@ function Calendar({}: Props) {
                           <span id="clickSpan" onClick={toggle} data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="block text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-blue-800 px-2 py-3 text-center cursor-pointer hover:text-blue-500">
                             {dayOfCurrentMonth}
                           </span>
-                          <Modal isOpen={isOpen} toggle={toggle}>
-                          <div data-modal-target="staticModal" data-modal-toggle="staticModal" id="defaultModal" tabIndex={-1} aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-                          <div className="relative w-full h-full max-w-2xl md:h-auto">
-
-                              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-
-                                  <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                                      <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                          Terms of Service
-                                      </h3>
-                                      <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-                                          <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                          <span className="sr-only">Close modal</span>
-                                      </button>
-                                  </div>
-
-                                  <div className="p-6 space-y-6">
-                                      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                          With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                                      </p>
-                                      <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                          The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                                      </p>
-                                  </div>
-
-                                  <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                                      <button data-modal-hide="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                                      <button data-modal-hide="defaultModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-                                  </div>
-                              </div>
-                          </div>
-                          </div>
-                        </Modal>
                         </div>
                         );
                       }
--- src/components/Calendar/Modals/Modal.tsx
+++ src/components/Calendar/Modals/Modal.tsx
@@ -1,42 +1,107 @@
-import { Fragment, ReactNode, useState } from "react";
-
-interface ModalType {
-    children?: ReactNode;
-    isOpen: boolean;
-    toggle: () => void;
-}
-
 function Modal() {
+    // Grabs all the Elements by their IDs which we had given them
+    let modal = document.getElementById("my-modal");
+
+    let btn = document.getElementById("open-btn");
+
+    let button = document.getElementById("ok-btn");
+
+    if(btn != null) {
+        btn.onclick = function() {
+            modal!.style.display = "block";
+            }
+            // We want the modal to close when the OK button is clicked
+            button!.onclick = function() {
+            modal!
+            .style.display = "none";
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal!.style.display = "none";
+                }
+                }
+            }
+    }
+    
     return (
         <>
-          <div id="staticModal" data-modal-backdrop="static" tabIndex={-1} aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-    <div className="relative w-full h-full max-w-2xl md:h-auto">
-
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-
-            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
-                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-                </button>
-            </div>
-
-            <div className="p-6 space-y-6">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-            </div>
-            <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="staticModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                <button data-modal-hide="staticModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-            </div>
+        <div className="fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal"></div>
+            <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+	            <div className="mt-3 text-center">
+		        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+			        <svg
+			        	className="h-6 w-6 text-green-600"
+			        	fill="none"
+			        	stroke="currentColor"
+			        	viewBox="0 0 24 24"
+			        	xmlns="http://www.w3.org/2000/svg"
+			        >
+				    <path
+				    	stroke-linecap="round"
+				    	stroke-linejoin="round"
+				    	stroke-width="2"
+				    	d="M5 13l4 4L19 7"
+				    ></path>
+			        </svg>
+		                </div>
+		                <h3 className="text-lg leading-6 font-medium text-gray-900">Successful!</h3>
+		                <div className="mt-2 px-7 py-3">
+		                	<p className="text-sm text-gray-500">
+		                		Account has been successfully registered!
+		                	</p>
+		                </div>
+		                <div className="items-center px-4 py-3">
+		                	<button
+		                		id="ok-btn"
+		                		className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+		                	>
+		                		OK
+		                	</button>
+		                </div>
+	        </div>
         </div>
-    </div>
-</div>
+          {/* <div id="extralarge-modal" tabIndex={-1} className="fixed top-0 left-0 right-0 z-50 w-full hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+            <div className="relative w-full h-full max-w-7xl md:h-auto">
+
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                    <div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+                            Extra Large modal
+                        </h3>
+                        <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="extralarge-modal">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+
+                    <div className="p-6 space-y-6">
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                        </p>
+                    </div>
+
+                    <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                        <button data-modal-hide="extralarge-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                        <button data-modal-hide="extralarge-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                    </div>
+                </div>
+            </div>
+</div> */}
         </>
       );      
 }
